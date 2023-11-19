d02c79e6b65ec9a95853ae9093e0116834c076e2
Kenny <kennypluchau@gmail.com>
2023-11-18 19:36:57 -0800

    Complete

--- 11-6/ContentView.swift
+++ 11-6/ContentView.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     @State var changeColor = false
-    @State var message = ""
-    var placeholder: LocalizedStringKey = "placeholder-label"
+    @State var name = ""
+    @State var buttonNumber = 1
+    let placeholder: LocalizedStringKey = "placeholder-label"
+    let value = 3.1415
+    let date: Date
+    let dateFormatter: DateFormatter
+    init() {
+        date = Date()
+        dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .medium
+    }
     var body: some View {
         VStack {
-            Text("greeting-label")
-            Button("button-label", action: {
+            Text("greeting-label \(name)")
+            Text("pi-label \(value)")
+            Button("\(buttonNumber) button-label", action: {
                 changeColor.toggle()
+                buttonNumber += 1
             })
-            TextField(placeholder, text: $message)
+            TextField(placeholder, text: $name)
+            Text(date, formatter: dateFormatter)
         }.background(changeColor ? Color.red : Color.yellow)
+        VStack {
+                    Text("Hello, world!")
+                        .padding()
+                        .background(Color.yellow)
+                    Text("This is a lot of text to display in a small amount of space to test how well Xcode can truncate large amounts of text.")
+                        .padding()
+                        .background(Color.mint)
+        //                .lineLimit(2)
+                }
     }
 }
 
@@ -28,7 +50,7 @@ struct ContentView_Previews: PreviewProvider {
             ContentView()
                 .environment(\.locale, .init(identifier: "en"))
             ContentView()
-                .environment(\.locale, .init(identifier: "jp"))
+                .environment(\.locale, .init(identifier: "ja"))
         }
     }
 }
